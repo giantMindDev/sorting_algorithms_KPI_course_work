a997acbbc9d4a3d1a54ec6868b4c916b685c038b
Oleh Kuz <olegkuz88@gmail.com>
2024-03-21 12:34:09 +0200

    Revert "Revert "sorting_algorithms_course_work""
    This reverts commit 1fe5ce01bd5d26292b9c9744047b010e64c22971.

---
+++ bucketSort.h
@@ -0,0 +1,199 @@
+#ifndef BUCKETSORT_H
+#define BUCKETSORT_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+#include "facilities.h"
+
+using namespace std;
+
+// Допоміжні глобальні змінни для фіксації кількості порівнянь та обмініві
+int bucket_compare = 0;
+int bucket_exchange = 0;
+
+#define NBUCKET 10  // Кількість комірок
+#define INTERVAL 10  // Ємність кожної комірки
+
+struct Node {
+  int data;
+  struct Node *next;
+};
+
+void bucketSort(int arr[], int size);
+struct Node *insertionSort(struct Node *list);
+int getBucketIndex(int value);
+
+// Алгоритм блочного сортування
+void bucketSort(int arr[], int size) {
+  int i, j;
+  struct Node **buckets;
+
+  // Створення комірок та розподілення пам'яті
+  buckets = (struct Node **)malloc(sizeof(struct Node *) * NBUCKET);
+
+  // Ініціалізація порожніх комірок
+  for (i = 0; i < NBUCKET; ++i) {
+    buckets[i] = NULL;
+  }
+
+  // Наповнення комірок відповідними елементами
+  for (i = 0; i < size; ++i) {
+    struct Node *current;
+    int pos = getBucketIndex(arr[i]);
+    current = (struct Node *)malloc(sizeof(struct Node));
+    current->data = arr[i];
+    current->next = buckets[pos];
+    buckets[pos] = current;
+    bucket_exchange++;
+  }
+
+  // Сортування елементів кожної комірки методом включення
+  for (i = 0; i < NBUCKET; ++i) {
+    buckets[i] = insertionSort(buckets[i]);
+  }
+
+  // Перенос відсортованих елементів в основний масив
+  for (j = 0, i = 0; i < NBUCKET; ++i) {
+    struct Node *node;
+    node = buckets[i];
+    while (node) {
+      arr[j++] = node->data;
+      node = node->next;
+    }
+  }
+
+  // Очистка комірок
+  for (i = 0; i < NBUCKET; ++i) {
+    struct Node *node;
+    node = buckets[i];
+    while (node) {
+      struct Node *tmp;
+      tmp = node;
+      node = node->next;
+      free(tmp);
+    }
+  }
+  free(buckets);
+  return;
+}
+
+// Алгоритм сортування включенням
+struct Node *insertionSort(struct Node *list) {
+  struct Node *k, *nodeList;
+  if (list == 0 || list->next == 0) {
+    return list;
+  }
+
+  nodeList = list;
+  k = list->next;
+  nodeList->next = 0;
+  while (k != 0) {
+    struct Node *ptr;
+    if (nodeList->data > k->data) {
+      struct Node *tmp;
+      tmp = k;
+      k = k->next;
+      tmp->next = nodeList;
+      nodeList = tmp;
+      continue;
+    }
+
+    bucket_exchange++;
+
+    for (ptr = nodeList; ptr->next != 0; ptr = ptr->next) {
+      bucket_compare++;
+      if (ptr->next->data > k->data) {
+        break;
+      }
+    }
+
+    if (ptr->next != 0) {
+      struct Node *tmp;
+      tmp = k;
+      k = k->next;
+      tmp->next = ptr->next;
+      ptr->next = tmp;
+      continue;
+    } else {
+      ptr->next = k;
+      k = k->next;
+      ptr->next->next = 0;
+      continue;
+    }
+  }
+  return nodeList;
+}
+
+// Визначення індексу комірки
+int getBucketIndex(int value) {
+  return value / INTERVAL;
+}
+
+// Запуск функції сортування підрахунком
+void runBucketSort(int demo = 0) {
+
+  string rawArray;
+
+  switch(demo) {
+    case 1:
+      cout << "Bucket Sort - demo 1000 ASC" << endl;
+      rawArray = arrayFromFile("testArrays/bucket1000asc.txt");
+      break;
+    case 2:
+      cout << "Bucket Sort - demo 1000 DESC" << endl;
+      rawArray = arrayFromFile("testArrays/bucket1000desc.txt");
+      break;
+    case 3:
+      cout << "Bucket Sort - demo 1000 random" << endl;
+      rawArray = randomArray(1000, 100);
+      break;
+    case 4:
+      cout << "Bucket Sort - demo 10000 ASC" << endl;
+      rawArray = arrayFromFile("testArrays/bucket10000asc.txt");
+      break;
+    case 5:
+      cout << "Bucket Sort - demo 10000 DESC" << endl;
+      rawArray = arrayFromFile("testArrays/bucket10000desc.txt");
+      break;
+    case 6:
+      cout << "Bucket Sort - demo 10000 random" << endl;
+      rawArray = randomArray(10000, 100);
+      break;
+    case 7:
+      cout << "Bucket Sort - demo 100000 ASC" << endl;
+      rawArray = arrayFromFile("testArrays/bucket100000asc.txt");
+      break;
+    case 8:
+      cout << "Bucket Sort - demo 100000 DESC" << endl;
+      rawArray = arrayFromFile("testArrays/bucket100000desc.txt");
+      break;
+    case 9:
+      cout << "Bucket Sort - demo 100000 random" << endl;
+      rawArray = randomArray(100000, 100);
+      break;
+    default:
+      rawArray = handArray();
+  }
+
+  vector<int> futureArray = arrayFromInputInt(rawArray);
+  int arr[futureArray.size()];
+  for (int i=0; i < futureArray.size(); i++) { 
+    arr[i] = futureArray[i];
+  }
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  cout << "Bucket Sort start" << endl;
+  bucketSort(arr, n);
+  outputFile(arr, n);
+
+  cout << "Compares = " << bucket_compare << endl;
+  cout << "Exchanges = " << bucket_exchange << endl;
+  bucket_compare = 0;
+  bucket_exchange = 0;
+
+  return;
+}
+
+#endif
---
+++ radixSort.h
@@ -0,0 +1,145 @@
+#ifndef RADIXSORT_H
+#define RADIXSORT_H
+
+#include <iostream>
+#include <vector>
+
+#include "facilities.h"
+
+using namespace std;
+
+// Допоміжні глобальні змінни для фіксації кількості порівнянь та обмініві
+int radix_compare = 0;
+int radix_exchange = 0;
+
+// Функція пошуку найбільшого значення в масиві
+int getMaxRadix(int array[], int n) {
+  int max = array[0];
+  for (int i = 1; i < n; i++)
+    if (array[i] > max)
+      max = array[i];
+  return max;
+}
+
+// Алгоритм сортування підрахунком для кожного окремого розряду
+void countingForRasixSort(int array[], int size, int place) {
+  
+  // Ініціалізація вихідного масиву (розмір + 1)
+  int output[size + 1];
+
+  // Пошук найбільшого елемента масиву
+  int max = (array[0] / place) % 10;
+  for (int i = 1; i < size; i++) {
+    if (((array[i] / place) % 10) > max)
+      max = array[i];
+  }
+
+  // Ініціалізація масиву з лічильниками розміром (max+1)
+  int count[max + 1];
+
+  // Заповнення масиву з лічильниками значеннями 0
+  for (int i = 0; i < max; ++i) {
+    count[i] = 0;
+  }
+
+  // Підрахунок кожного елемента вхідного масиву у відповідному індексі масиву лічильників
+  for (int i = 0; i < size; i++) {
+    count[(array[i] / place) % 10]++;
+    radix_exchange++;
+  }
+
+  // Кумулятивна сума всіх попередніх значень в масиві лічильників
+  for (int i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+  }
+
+  // Пошук індекса кожного елемента вхідного масива в масиві лічильників
+  // Розміщення елементів в вихідний масив, декремент лічильника
+  for (int i = size - 1; i >= 0; i--) {
+    output[count[(array[i] / place) % 10] - 1] = array[i];
+    count[(array[i] / place) % 10]--;
+    radix_compare++;
+  }
+  
+  // Перенос відсортованих значень у вхідний масив
+  for (int i = 0; i < size; i++) {
+    array[i] = output[i];
+  }
+}
+
+// Алгоритм порозрядного сортування
+void radixSort(int array[], int size) { 
+  // Пошук найбільшого елементу
+  int max = getMaxRadix(array, size);
+  
+  // Застосування сортування підрахунком для впорядкування за кожним розрядом
+  for (int place = 1; max / place > 0; place *= 10)
+    countingForRasixSort(array, size, place);
+}
+
+// Запуск функції сортування підрахунком
+void runRadixSort(int demo = 0) {
+
+  string rawArray;
+
+  switch(demo) {
+    case 1:
+      cout << "Radix Sort - demo 1000 ASC" << endl;
+      rawArray = arrayFromFile("testArrays/radix1000asc.txt");
+      break;
+    case 2:
+      cout << "Radix Sort - demo 1000 DESC" << endl;
+      rawArray = arrayFromFile("testArrays/radix1000desc.txt");
+      break;
+    case 3:
+      cout << "Radix Sort - demo 1000 random" << endl;
+      rawArray = randomArray(1000, 1000);
+      break;
+    case 4:
+      cout << "Radix Sort - demo 10000 ASC" << endl;
+      rawArray = arrayFromFile("testArrays/radix10000asc.txt");
+      break;
+    case 5:
+      cout << "Radix Sort - demo 10000 DESC" << endl;
+      rawArray = arrayFromFile("testArrays/radix10000desc.txt");
+      break;
+    case 6:
+      cout << "Radix Sort - demo 10000 random" << endl;
+      rawArray = randomArray(10000, 1000);
+      break;
+    case 7:
+      cout << "Radix Sort - demo 100000 ASC" << endl;
+      rawArray = arrayFromFile("testArrays/radix100000asc.txt");
+      break;
+    case 8:
+      cout << "Radix Sort - demo 100000 DESC" << endl;
+      rawArray = arrayFromFile("testArrays/radix100000desc.txt");
+      break;
+    case 9:
+      cout << "Radix Sort - demo 100000 random" << endl;
+      rawArray = randomArray(100000, 1000);
+      break;
+    default:
+      rawArray = handArray();
+  }
+
+  vector<int> futureArray = arrayFromInputInt(rawArray);
+  int arr[futureArray.size()];
+  for (int i=0; i < futureArray.size(); i++) { 
+    arr[i] = futureArray[i];
+  }
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  cout << "Radix Sort start" << endl;
+  radixSort(arr, n);
+  outputFile(arr, n);
+
+  cout << "Compares = " << radix_compare << endl;
+  cout << "Exchanges = " << radix_exchange << endl;
+  radix_compare = 0;
+  radix_exchange = 0;
+
+  return;
+}
+
+#endif
